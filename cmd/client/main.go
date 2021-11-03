6a23935a7fae5acefdaa74c50715f668ccb121d0
Liubomyr Mykhalchyshyn <LMykhalchyshyn@slb.com>
2021-11-03 12:00:51 +0200

    upd messenger

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"github.com/Morzaka/project-layout/client"
-	"github.com/oleksandr-chornovol/lets-go-chat/pkg/hasher"
 )
 
 func main() {
-	if hasher.CheckPasswordHash("", "") {
-		println("that ok")
-	}
 	client.NewConnection()
 }
--- server/serv/handler.go
+++ server/serv/handler.go
@@ -1,9 +1,12 @@
 package serv
 
-import "github.com/Morzaka/project-layout/server/session"
+import (
+	"github.com/Morzaka/project-layout/client"
+	"github.com/Morzaka/project-layout/server/session"
+)
 
-func CreateNewRoom() {
+func CreateNewRoom(connection client.Connection) {
 
 	// if you allow creating new room than...
-	session.NewRoom()
+	session.NewRoom(connection, "")
 }
