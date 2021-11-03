d75617867f045c9aadccb60cad7ec42165531803
Liubomyr Mykhalchyshyn <LMykhalchyshyn@slb.com>
2021-11-03 11:52:45 +0200

    upd messenger

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,13 @@
-package client
+package main
 
-import "github.com/Morzaka/project-layout/client"
+import (
+	"github.com/Morzaka/project-layout/client"
+	"github.com/oleksandr-chornovol/lets-go-chat/pkg/hasher"
+)
 
 func main() {
+	if hasher.CheckPasswordHash("", "") {
+		println("that ok")
+	}
 	client.NewConnection()
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/Morzaka/project-layout
 
 go 1.16
 
-require github.com/google/uuid v1.3.0 // indirect
+require (
+	github.com/google/go-licenses v0.0.0-20211006200916-ceb292363ec8 // indirect
+	github.com/google/uuid v1.3.0
+	github.com/oleksandr-chornovol/lets-go-chat v0.0.0-20211101113433-e4b13a9aae86
+)
